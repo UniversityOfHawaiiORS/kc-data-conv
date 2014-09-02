0741a395f2888ad195408cd7429366baf1352a70
Travis Schneberger <leo.herbie@gmail.com>
2014-09-02 09:55:38 -0400

    first commit

---
+++ src/main/java/org/kuali/coeus/dac/Main.java
@@ -0,0 +1,11 @@
+package org.kuali.coeus.dac;
+
+/**
+ * The main driver program that runs this conversion program.
+ */
+public final class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
